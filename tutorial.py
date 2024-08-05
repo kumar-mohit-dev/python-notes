3fd15cb75f3501e0899690355535dde30559c2bd
𝙇𝙤𝙨𝙩𝘼𝙙𝙢𝙞𝙣 <45988483+LostAdmins@users.noreply.github.com>
2024-08-05 23:41:21 +0530

    Add files via upload

--- tutorial.py
+++ tutorial.py
@@ -632,4 +632,399 @@ print(car_3.color)
 print(bike_1.color)
 
 #duck typing
-#
+duck typing = concept where the class of an object is less important than the methods/attributes
+class type is not checked if minimum methods/attributes are present
+“If it walks like a duck, and it quacks like a duck, then it must be a duck.”
+class Duck:
+    def walk(self):
+        print("This duck is walking")
+    def talk(self):
+        print("This duck is qwuacking")
+class Chicken:
+    def walk(self):
+        print("This chicken is walking")
+    def talk(self):
+        print("This chicken is clucking")
+class Person():
+    def catch(self, duck):
+        duck.walk()
+        duck.talk()
+        print("You caught the critter!")
+duck = Duck()
+chicken = Chicken()
+person = Person()
+person.catch(chicken)
+
+#walrus operator 
+# assignment expression aka walrus operator
+# assigns values to variables as part of a larger expression
+ happy = True
+ print(happy) #output -- True
+ print(happy := True) # output -- True
+ foods = list() 
+ while True:
+   food = input("What food do you like?: ")
+       if food == "quit":
+           break
+   foods.append(food)
+foods = list() #now writing above code using walrus operator
+while (food := input("What food do you like?: ")) != "quit":
+    foods.append(food)
+
+#functions to variable
+def hello():
+    print("Hello")
+hi = hello
+hi() #output -- Hello
+say = print
+say("Whoa! I can't believe this works! :O") #output -- Whoa! I cant believe this works! :O
+
+#higher order functions
+#  Higher Order Function =  a function that either:
+#                           1. accepts a function as an argument
+#                               or
+#                           2. returns a function
+#                           (In python, functions are also treated as objects)
+
+# ----- 1. accepts a function as an argument ----- 
+def loud(text):
+   return text.upper()
+def quiet(text):
+   return text.lower()
+def hello(func):
+   text = func("Hello")
+   print(text)
+hello(loud)
+hello(quiet)
+# ------------ 2. returns a function ------------- 
+def divisor(x):
+   def dividend(y):
+       return y / x
+   return dividend
+divide = divisor(2)
+print(divide(10))
+
+#lambda
+# lambda function = function written in 1 line using lambda keyword
+#                   accepts any number of arguments, but only has one expression.
+#                   (think of it as a shortcut)
+#                   (useful if needed for a short period of time, throw-away)
+# lambda parameters:expression
+double = lambda x: x * 2 #lambda argument:return'
+print(double(1))
+multiply = lambda x, y: x * y
+print(multiply(1,2))
+add = lambda x, y, z: x + y + z
+print(add(1,2,3))
+full_name = lambda first_name, last_name: first_name+" "+last_name
+print(full_name("Bro","Code"))
+age_check= lambda age:True if age >= 18 else False
+
+#sort
+# sort() method   = used with lists
+# sort() function = used with iterables
+students = (("Squidward", "F", 60),
+            ("Sandy", "A", 33),
+            ("Patrick","D", 36),
+            ("Spongebob","B", 20),
+            ("Mr.Krabs","C", 78))
+grade = lambda grades:grades[1]
+# students.sort(key=age)                     # sorts current list
+sorted_students = sorted(students,key=grade) # sorts and creates a new list
+for i in sorted_students:
+    print(i)
+
+#map
+# map() =   applies a function to each item in an iterable (list, tuple, etc.)
+# map(function,iterable)
+store = [("shirt",20.00),
+         ("pants",25.00),
+         ("jacket",50.00),
+         ("socks",10.00)]
+to_euros = lambda data: (data[0],data[1]*0.82)
+# to_dollars = lambda data: (data[0],data[1]/0.82)this represents tuples
+store_euros = list(map(to_euros, store))
+for i in store_euros:
+    print(i)
+
+#filter
+# filter() =    creates a collection of elements from an iterable,
+#               for which a function returns true
+#               filter(function, iterable)
+friends = [("Rachel",19),
+           ("Monica",18),
+           ("Phoebe",17),
+           ("Joey",16),
+           ("Chandler",21),
+           ("Ross",20)]
+age = lambda data:data[1] >= 18
+drinking_buddies = list(filter(age, friends))
+for i in drinking_buddies:
+    print(i)
+
+#reduce
+# reduce() = apply a function to an iterable and reduce it to a single cumulative value.
+#            performs function on first two elements and repeats process until 1 value remains
+# reduce(function, iterable)
+import functools
+letters = ["H","E","L","L","O"]
+word = functools.reduce(lambda x, y,:x + y,letters)
+print(word)
+# factorial = [5,4,3,2,1]
+# result = functools.reduce(lambda x, y,:x * y,factorial)
+# print(result)
+
+#list comprehensions
+# list comprehension =  a way to create a new list with less syntax
+#                       can mimic certain lambda functions, easier to read
+#                       list = [expression for item in iterable]
+#                       list = [expression for item in iterable if conditional]
+#                       list = [expression if conditional else for item in iterable]
+# --------------------------------------------------------------
+squares = []                # create an empty list
+for i in range(1,11):       # create a for loop
+    squares.append(i * i)    # define what each loop iteration should do
+print(squares)
+# create a list AND defines what each loop iteration should do
+squares = [i * i for i in range(1,11)]
+print(squares)
+
+#dictionary comprehensions
+# dictionary comprehension = create dictionaries using an expression
+#                            can replace for loops and certain lambda functions
+# dictionary = {key: expression for (key,value) in iterable}
+# dictionary = {key: expression for (key,value) in iterable if conditional}
+# dictionary = {key: (if/else) for (key,value) in iterable}
+# dictionary = {key: function(value) for (key,value) in iterable}
+# -------------------------------------------------------------------------
+cities_in_F = {'New York': 32, 'Boston': 75, 'Los Angeles': 100, 'Chicago': 50}
+cities_in_C = {key: round((value-32)*(5/9)) for (key,value) in cities_in_F.items()
+print(cities_in_C)
+# -------------------------------------------------------------------------
+# weather = {'New York': "snowing", 'Boston': "sunny", 'Los Angeles': "sunny", 'Chicago': "cloudy"}
+# sunny_weather = {key: value for (key,value) in weather.items() if value == "sunny"}
+# print(sunny_weather)
+
+#zip function
+# zip(*iterables) =  aggregate elements from two or more iterables (list, tuples, sets, etc.)
+#                               creates a zip object with paired elements stored in tuples for each element
+usernames = ["Dude", "Bro", "Mister"]
+passwords = ("p@ssword", "abc123", "guest")
+login_dates = ["1/1/2021","1/2/2021","1/3/2021"]
+# --------------------------------------
+users = list(zip(usernames,passwords))
+for i in users:
+    print(i)
+# --------------------------------------
+users = dict(zip(usernames,passwords))
+for key,value in users.items():
+    print(key+" : "+value)
+# --------------------------------------
+users = zip(usernames,passwords,login_dates)
+for i in users:
+    print(i)
+
+#if _name_ =='__main__'
+# ***********************************
+# y tho?
+# 1. Module can be run as a standalone program
+#    or
+# 2. Module can be imported and used by other modules
+#  Python interpreter sets "special variables", one of which is __name__
+#  Python will assign the __name__ variable a value of '__main__' if it's
+#  the initial module being run
+def main():
+    print("Hello!")
+if __name__ == '__main__':
+    main()
+
+#time module
+import time
+# ***************************************************************************
+print(time.ctime(0))    # convert a time expressed in seconds since epoch to a readable string
+#                                        epoch = when your computer thinks time began (reference point)
+print(time.time())      # return current seconds since epoch
+print(time.ctime(time.time())) # will get current time
+# ***************************************************************************
+# time.strftime(format, time_object) = formats a time_object to a string
+# time_object = time.localtime() # local time
+# time_object = time.gmtime()  # UTC time
+# local_time = time.strftime("%B %d %Y %H:%M:%S", time_object)
+# print(local_time)
+# ***************************************************************************
+# time.strptime(string_string, format) = parses a string representing time/date and returns a struct_time object
+# time_string = "20 April, 2020"
+# time_object = time.strptime(time_string,"%d %B, %Y")
+# print(time_object)
+# ***************************************************************************
+# time.asctime(time_tuple) = accepts a time_object or a tuple up to 9 elements and returns a string
+# (year, month, day, hours, minutes, secs, #day​ of the week, #day​ of the year, dst)
+# time_tuple = (2020, 4, 20, 4, 20, 0, 0, 0, 0)
+# time_string = time.asctime(time_tuple)
+# print(time_string)
+# ***************************************************************************
+# time.asctime(time_tuple) = accepts a time_object or a tuple up to 9 elements and return seconds since epoch
+# (year, month, day, hours, minutes, secs, #day​ of the week, #day​ of the year, dst)
+# time_tuple = (2020, 4, 20, 4, 20, 0, 0, 0, 0)
+# time_string = time.mktime(time_tuple)
+# print(time_string)
+
+#threading
+# thread =  a flow of execution. Like a separate order of instructions.
+#                  However each thread takes a turn running to achieve concurrency
+#                  GIL = (global interpreter lock),
+#                  allows only one thread to hold the control of the Python interpreter at any one time
+# cpu bound = program/task spends most of its time waiting for internal events (CPU intensive)
+#                        use multiprocessing
+# io bound = program/task spends most of its time waiting for external events (user input, web scraping)
+#                     use multithreading
+import threading
+import time
+def eat_breakfast():
+    time.sleep(3)
+    print("You eat breakfast")
+def drink_coffee():
+    time.sleep(4)
+    print("You drank coffee")
+def study():
+    time.sleep(5)
+    print("You finish studying")
+x = threading.Thread(target=eat_breakfast, args=())
+x.start()
+y = threading.Thread(target=drink_coffee, args=())
+y.start()
+z = threading.Thread(target=study, args=())
+z.start()
+x.join()
+y.join()
+z.join()
+print(threading.active_count())
+print(threading.enumerate())
+print(time.perf_counter())
+
+#daemon threads
+# daemon thread = a thread that runs in the background, not important for program to run
+#                 your program will not wait for daemon threads to complete before exiting
+#                 non-daemon threads cannot normally be killed, stay alive until task is complete
+#                 ex. background tasks, garbage collection, waiting for input, long running processes
+import threading
+import time
+def timer():
+    print()
+    count = 0
+    while True:
+        time.sleep(1)
+        count += 1
+        print("logged in for: ", count, "seconds")
+x = threading.Thread(target=timer, daemon=True)
+x.start()
+# x.setDaemon(True)
+# print(x.isDaemon())
+answer = input("Do you wish to exit?")
+
+#multiprocessing
+# multiprocessing = running tasks in parallel on different cpu cores, bypasses GIL used for threading
+#                   multiprocessing = better for cpu bound tasks (heavy cpu usage)
+#                   multithreading = better for io bound tasks (waiting around)
+from multiprocessing import Process, cpu_count
+import time
+def counter(num):
+    count = 0
+    while count < num:
+        count += 1
+def main():
+    print("cpu count:", cpu_count())
+    a = Process(target=counter, args=(500000000,))
+    b = Process(target=counter, args=(500000000,))
+    a.start()
+    b.start()
+    print("processing...")
+    a.join()
+    b.join()
+    print("Done!")
+    print("finished in:", time.perf_counter(), "seconds")
+if _name_ == '__main__':
+    main()
+
+#gui windows 
+from tkinter import *
+window = Tk() #instantiate an instance of a window
+window.geometry("420x420")
+window.title("Bro Code first GUI program")
+icon = PhotoImage(file='logo.png')
+window.iconphoto(True,icon)
+window.config(background="#5cfcff")
+window.mainloop() #place window on computer screen, listen for events
+
+#labels
+from tkinter import *
+# label = an area widget that holds text and/or an image within a window
+window = Tk()
+photo = PhotoImage(file='person.png')
+label = Label(window,
+              text="bro, do you even code?",
+              font=('Arial',40,'bold'),
+              fg='#00FF00',
+              bg='black',
+              relief=RAISED,
+              bd=10,
+              padx=20,
+              pady=20,
+              image=photo,
+              compound='bottom')
+label.pack()
+#label.place(x=0,y=0)
+window.mainloop()
+
+#buttons
+from tkinter import *
+# button = you click it, then it does stuff
+count = 0
+def click():
+    global count
+    count+=1
+    print(count)
+window = Tk()
+photo = PhotoImage(file='like.png')
+button = Button(window,
+                text="click me!",
+                command=click,
+                font=("Comic Sans",30),
+                fg="#00FF00",
+                bg="black",
+                activeforeground="#00FF00",
+                activebackground="black",
+                state=ACTIVE,
+                image=photo,
+                compound='bottom')
+button.pack()
+window.mainloop()
+
+#entrybox
+from tkinter import *
+#entry widget = textbox that accepts a single line of user input
+def submit():
+    username = entry.get()
+    print("Hello "+username)
+def delete():
+    entry.delete(0,END)
+def backspace():
+    entry.delete(len(entry.get())-1, END)
+window = Tk()
+entry = Entry(window,
+              font=("Arial",50),
+              fg="#00FF00",
+              bg="black")
+#entry.insert(0,'Spongebob')
+#entry.config(show="*")
+#entry.config(state=DISABLED)
+entry.pack(side=LEFT)
+submit_button = Button(window,text="submit",command=submit)
+submit_button.pack(side=RIGHT)
+delete_button = Button(window,text="delete",command=delete)
+delete_button.pack(side=RIGHT)
+backspace_button = Button(window,text="backspace",command=backspace)
+backspace_button.pack(side=RIGHT)
+window.mainloop()
+
+
